e918939700c42bfad2c5036a721b2f99f3b07f20
Michelle Kiziak <makiziak@gmail.com>
2016-11-02 20:15:30 -0700

    changes to AddUserIdGroup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102212117) do
+ActiveRecord::Schema.define(version: 20161103025723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161102212117) do
     t.string   "group_email"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "user_id"
   end
 
   create_table "my_houses", force: :cascade do |t|
