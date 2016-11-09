b4354a3f3f36e06d9ea00d7f7414db9271ebd25b
Michelle Kiziak <makiziak@gmail.com>
2016-11-09 13:15:11 -0800

    moved logout to top of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #get "users", to: "users#index"
+  delete 'logout', to: 'sessions#destroy'
+  resource :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resources :events
-  resource :sessions, only: [:new, :create, :destroy]
   resources :groups
   root "events#index"
 
   get 'login', to: 'sessions#new'
-  delete 'logout', to: 'sessions#destroy'
 
 end
 
