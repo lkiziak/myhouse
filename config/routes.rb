5e68b96c140ba0b7ed2af32056409ffc88a1fbef
Luis Metzger <luismetzger@me.com>
2016-11-08 21:01:29 -0700

    change delete route to a GET route.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   # delete '/logout', to: 'sessions#destroy'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 
 
 end
