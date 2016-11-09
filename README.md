17aa41f62f47dd785f29b5896d6fd58a8ebb89b8
Michelle Kiziak <makiziak@gmail.com>
2016-11-09 12:58:18 -0800

    change logout to delete req

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# README
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- # README
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
@@ -22,3 +50,4 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+ -->
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -8,37 +8,37 @@ This file should contain all the record creation needed to seed the database wit
 #   Event.create(title: "Concert", description: "Community concert", location: "Riverside", contact: "La Sierra")
 
 
-User.destroy_all
-Event.destroy_all
+# User.destroy_all
+# Event.destroy_all
 
-30.times do ||
+# 30.times do ||
 
-x = ::Faker::User.user_name
-y = ::Faker::User.email
-z = ::Faker::User.password(8)
-#a = ::Faker::Internet.url #website
-#b = ::Faker::Lorem.sentence #about
-#c = ::Faker::Avatar.image #img_url
+# x = ::Faker::User.user_name
+# y = ::Faker::User.email
+# z = ::Faker::User.password(8)
+# #a = ::Faker::Internet.url #website
+# #b = ::Faker::Lorem.sentence #about
+# #c = ::Faker::Avatar.image #img_url
 
-User.create({name: x, email: y, password: z, password_confirmation: z})
+# User.create({name: x, email: y, password: z, password_confirmation: z})
 
-#website: a, about: b, img_url: c
-end
-
-# 20.times do ||
-#     30.times do |i|
-
-#     id = User.first.id + i
-#     x = Faker::group_name.words(2) #title
-#     #y = ::Faker::Number.between(0, 100000) #plays
-#     #a = ::Faker::Internet.url #website
-#     #b = ::Faker::Lorem.paragraph #about
-#     #c = ::Faker::Number.between(0, 300) #length
-
-#     Group.create({group_name: id, group_email: y})
-
-#     end
+# #website: a, about: b, img_url: c
 # end
+
+# # 20.times do ||
+# #     30.times do |i|
+
+# #     id = User.first.id + i
+# #     x = Faker::group_name.words(2) #title
+# #     #y = ::Faker::Number.between(0, 100000) #plays
+# #     #a = ::Faker::Internet.url #website
+# #     #b = ::Faker::Lorem.paragraph #about
+# #     #c = ::Faker::Number.between(0, 300) #length
+
+# #     Group.create({group_name: id, group_email: y})
+
+# #     end
+# # end
 
 
 
